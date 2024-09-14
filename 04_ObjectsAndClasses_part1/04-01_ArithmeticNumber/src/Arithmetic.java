55065a6e6f6befeb84a6a193ddb6afd1794f483c
oleg-programmistjava <programmistjava@yandex.ru>
2024-09-14 16:31:42 +0300

    Дописал в классе Arithmetic отдельные методы для операций суммирования чисел, получения их произведения, а также в общем методе(printAllArithmetics), который выводит все результаты внедрил другие методы, чтобы исключить дублирование кода.

--- 04_ObjectsAndClasses_part1/04-01_ArithmeticNumber/src/Arithmetic.java
+++ 04_ObjectsAndClasses_part1/04-01_ArithmeticNumber/src/Arithmetic.java
@@ -11,21 +11,41 @@ public class Arithmetic {
     }
 
     public void printAllArithmetics() {
+
+        printSumm();
+
+        printComposition();
+
+        System.out.println("---");
+
+        printComparingNumbers();
+
+
+    }
+
+    public void printSumm() {
+
         System.out.println("СУММА переданных чисел: " + (a+b) + " | (" + a + " + " + b + ")");
+
+    }
+
+    public void printComposition() {
+
         System.out.println("ПРОИЗВЕДЕНИЕ переданных чисел: " + a*b + " | (" + a + " * " + b + ")");
 
         System.out.println("---");
+    }
+
+    public void printComparingNumbers() {
 
         System.out.println("СРАВНЕНИЕ ЧИСЕЛ ПОКАЗАЛО, ЧТО: ");
 
         if (a > b) {
             System.out.println("- первое число больше, чем второе");
         }
-
         if (a < b ) {
             System.out.println("- второе число больше, чем первое");
         }
-
         if (a == b) {
             System.out.println("- первое и второе число равны");
         }
